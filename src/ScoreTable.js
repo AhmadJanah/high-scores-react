1314dd33fc2c0a8ad9aab15f0ae46da11dc681f4
AhmadJanah <ahmadjn82@hotmail.com>
2021-04-27 01:39:37 +0100

    My third commit

--- src/ScoreTable.js
+++ src/ScoreTable.js
@@ -2,7 +2,7 @@ import React from "react";
 function ScoreTable(props) {
     return (
         <div className="comp-div">
-            <h1>High Scores Per Country</h1>
+            <h1>High Scores Per Country:</h1>
             {props.tableScores.map(score => (
                 <div key={score.name} className="score-div">
                     <h2>HIGH SCORES: {score.name}</h2>
